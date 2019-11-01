e2e6e69231ae63b2fad5d7e7017873d5087f4b3b
Ezequiel Moreno <roberto.ezequiel@pwc.com>
2019-11-01 14:34:44 -0300

    Coors Updarte

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ const quizGeneratorController: QuizGeneratorController = new QuizGeneratorContro
 app.set("port", process.env.PORT || 3000);
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://nodecourse.z15.web.core.windows.net/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "https://nodecourse.z15.web.core.windows.net"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
